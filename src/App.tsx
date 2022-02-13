e6d8415d65fbccc3f23c76063d3944e1636f4a4c
Henry Fidlow <henryrockford36@yahoo.com>
2022-02-12 20:56:01 -0500

    solved-html-css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                Henry Fidlow UD CISC275 with React Hooks and TypeScript
+                <span style={{ color: "yellow" }}>
+                    Henry Fidlow UD CISC275 with React Hooks and TypeScript
+                </span>
+                <img
+                    src="https://i.pinimg.com/originals/c2/18/a0/c218a0dd7c4829d03d1e89cb824f3459.jpg"
+                    alt="Objectively the best snack"
+                    width="200"
+                    height="200"
+                />
             </header>
-            <p>Hello World</p>
+
+            <Container>
+                <Row>
+                    <Col>
+                        <h2>About me:</h2>
+                        <ol>
+                            <li>Senior Mechanical Engineer (CS minor)</li>
+                            <li>Interest in environmental stewardship</li>
+                            <li>Always sleepy!!!</li>
+                        </ol>
+                        <div id="rectangle"></div>
+                    </Col>
+                    <Col>
+                        <Button onClick={() => console.log("Hello World!")}>
+                            Log Hello World
+                        </Button>
+                        <div id="rectangle"></div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
