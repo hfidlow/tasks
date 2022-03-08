2bfbc9e38490cd4fb9f674a043e5a63c56fd1b2c
Henry Fidlow <henryrockford36@yahoo.com>
2022-03-07 19:36:12 -0500

    completed first 3 components

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,36 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
+
+type ChangeEvent = React.ChangeEvent<
+    HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+>;
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [answer, changeAnswer] = useState<string>("");
+
+    function updateAnswer(event: ChangeEvent) {
+        changeAnswer(event.target.value);
+    }
+
     return (
         <div>
             <h3>Check Answer</h3>
+
+            <Form.Group as={Row} controlId="answer-area">
+                <Form.Label column lg={6}>
+                    Howdy! Have you been keeping up with arithmetic? Surpise
+                    test!! Whats 6*6+66-(6*2-2)*6?
+                </Form.Label>
+                <Col lg={2}>
+                    <Form.Control value={answer} onChange={updateAnswer} />
+                </Col>
+            </Form.Group>
+            <div>{answer === expectedAnswer ? "✔️" : "❌"}</div>
         </div>
     );
 }
--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,43 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): JSX.Element {
+    const [edit, setEdit] = useState<boolean>(false);
+    const [student, setStudent] = useState<boolean>(true);
+    const [name, setName] = useState<string>("Your Name");
+
     return (
         <div>
             <h3>Edit Mode</h3>
+            <Form.Group>
+                <Form.Label>Enter your name.</Form.Label>
+                <Form.Control
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    disabled={!edit}
+                ></Form.Control>
+            </Form.Group>
+
+            <Form.Check
+                type="switch"
+                id="in-edit"
+                label="Edit Mode"
+                checked={edit}
+                onChange={(e) => setEdit(e.target.checked)}
+            />
+            <Form.Check
+                type="switch"
+                id="is-student"
+                label="Student"
+                checked={student}
+                onChange={(e) => setStudent(e.target.checked)}
+                disabled={!edit}
+            />
+            <div>
+                {student
+                    ? [name + " is a student"]
+                    : [name + " is not a student"]}
+            </div>
         </div>
     );
 }
