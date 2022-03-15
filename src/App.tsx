d33c6c163a75b79c25f981634b69e5c789e6e936
Henry Fidlow <henryrockford36@yahoo.com>
2022-03-15 16:22:01 -0400

    Modified added images

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from "react";
 //import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 import { ShowHideTasks } from "./ShowHide";
+import img1 from "./Images/img1.png";
 
 function App(): JSX.Element {
     return (
         <div>
             <ShowHideTasks></ShowHideTasks>
-            <img src="./quizzer/Images/T11_quiz_edit.JPG" />
+            <div>
+                <img src={img1} alt="img1" />
+            </div>
         </div>
     );
 }
