14668de540412cf60369fa44e4323d07f01d9b6e
Henry Fidlow <henryrockford36@yahoo.com>
2022-03-15 15:00:00 -0400

    Addd ShowHideTasks and relevant images for Task 11

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,13 @@
 import React from "react";
 //import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
-import { ChangeType } from "./components/ChangeType";
-import { RevealAnswer } from "./components/RevealAnswer";
-import { StartAttempt } from "./components/StartAttempt";
-import { TwoDice } from "./components/TwoDice";
-import { CycleHoliday } from "./components/CycleHoliday";
-import { Counter } from "./components/Counter";
-import { DoubleHalf } from "./bad-components/DoubleHalf";
-import { ColoredBox } from "./bad-components/ColoredBox";
-import { ShoveBox } from "./bad-components/ShoveBox";
-import { ChooseTeam } from "./bad-components/ChooseTeam";
-import { CheckAnswer } from "./form-components/CheckAnswer";
-import { GiveAttempts } from "./form-components/GiveAttempts";
-import { EditMode } from "./form-components/EditMode";
-import { MultipleChoiceQuestion } from "./form-components/MultipleChoiceQuestion";
-import { ChangeColor } from "./form-components/ChangeColor";
-import { Quizzer } from "./quizzer/Quizzer";
+import { ShowHideTasks } from "./ShowHide";
 
 function App(): JSX.Element {
     return (
-        <div className="App">
-            <header className="App-header">
-                <span style={{ color: "yellow" }}>
-                    Henry Fidlow UD CISC275 with React Hooks and TypeScript
-                </span>
-                <img
-                    src="https://i.pinimg.com/originals/c2/18/a0/c218a0dd7c4829d03d1e89cb824f3459.jpg"
-                    alt="Objectively the best snack"
-                    width="200"
-                    height="200"
-                />
-            </header>
-            <Quizzer></Quizzer>
-            <hr></hr>
-            <CheckAnswer expectedAnswer="42"></CheckAnswer>
-            <hr></hr>
-            <GiveAttempts></GiveAttempts>
-            <hr></hr>
-            <EditMode></EditMode>
-            <hr></hr>
-            <ChangeColor></ChangeColor>
-            <hr></hr>
-            <MultipleChoiceQuestion
-                options={["a", "b", "c"]}
-                expectedAnswer="b"
-            ></MultipleChoiceQuestion>
-            <hr></hr>
-            <DoubleHalf></DoubleHalf>
-            <hr></hr>
-            <ChooseTeam></ChooseTeam>
-            <hr></hr>
-            <ColoredBox></ColoredBox>
-            <hr></hr>
-            <ShoveBox></ShoveBox>
-            <hr></hr>
-            <Counter></Counter>
-            <hr />
-            Jackie Robinson, the famous baseball player and civil rights
-            pioneer, is the main character of this numerically titled movie.
-            <RevealAnswer></RevealAnswer>
-            <hr />
-            <StartAttempt></StartAttempt>
-            <hr />
-            <TwoDice></TwoDice>
-            <hr />
-            <ChangeType></ChangeType>
-            <hr />
-            <CycleHoliday></CycleHoliday>
+        <div>
+            <ShowHideTasks></ShowHideTasks>
+            <img src="./quizzer/Images/T11_quiz_edit.JPG" />
         </div>
     );
 }
