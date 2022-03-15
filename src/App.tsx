10ad1b15fe736ad148483bb74df566085b7d7bae
Henry Fidlow <henryrockford36@yahoo.com>
2022-03-15 16:42:04 -0400

    Same as previous commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,23 @@ import React from "react";
 import "./App.css";
 import { ShowHideTasks } from "./ShowHide";
 import img1 from "./Images/img1.png";
+import img2 from "./Images/img2.png";
+import img3 from "./Images/img3.png";
+import img4 from "./Images/img4.png";
 
 function App(): JSX.Element {
     return (
-        <div>
+        <div className="App">
             <ShowHideTasks></ShowHideTasks>
             <div>
-                <img src={img1} alt="img1" />
+                <img src={img1} alt="img1" width={300} height={380} />
+                <img src={img2} alt="img2" width={300} height={380} />
+                <img src={img3} alt="img3" width={300} height={380} />
+                <img src={img4} alt="img4" width={300} height={380} />
+            </div>
+            <div>
+                Completed Requirements:
+                <li> Added sketch</li>
             </div>
         </div>
     );
