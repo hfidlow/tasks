5b1a3affa8bdfa6b56e8cd4712704dc4ad04020a
Henry Fidlow <henryrockford36@yahoo.com>
2022-03-15 21:09:54 -0400

    Commented out images for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import React from "react";
 //import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 import { ShowHideTasks } from "./ShowHide";
-import img1 from "./Images/img1.png";
-import img2 from "./Images/img2.png";
-import img3 from "./Images/img3.png";
-import img4 from "./Images/img4.png";
+// import img1 from "./Images/img1.png";
+// import img2 from "./Images/img2.png";
+// import img3 from "./Images/img3.png";
+// import img4 from "./Images/img4.png";
 
 function App(): JSX.Element {
     return (
@@ -22,12 +22,12 @@ function App(): JSX.Element {
                 />
             </header>
             <ShowHideTasks></ShowHideTasks>
-            <div>
+            {/* <div>
                 <img src={img1} alt="img1" width={300} height={380} />
                 <img src={img2} alt="img2" width={300} height={380} />
                 <img src={img3} alt="img3" width={300} height={380} />
                 <img src={img4} alt="img4" width={300} height={380} />
-            </div>
+            </div> */}
             <div>
                 Completed Requirements:
                 <li> Added sketch</li>
