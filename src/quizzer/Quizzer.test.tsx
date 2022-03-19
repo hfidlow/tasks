77998cd700475cb76599292cb07f005f35b5dc52
Henry Fidlow <henryrockford36@yahoo.com>
2022-03-19 19:23:02 -0400

    Alter Quizzer files to allow for testing

--- src/quizzer/Quizzer.test.tsx
+++ src/quizzer/Quizzer.test.tsx
@@ -1,14 +1,35 @@
 import React from "react";
-import { render } from "@testing-library/react";
-//import { screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { Quizzer } from "./Quizzer";
 
 describe("Quizzer Tests", () => {
     beforeEach(() => {
         render(<Quizzer />);
     });
-    test("The Quizzer renders", () => {
-        // Up to you to decide what your tests are!
-        // Add more tests, more components, more test files!
+    test("Visible Quizzes (title, desc, # of questions)", () => {
+        const Quizzes = screen.getAllByRole("Quiz");
+        const firstQuiz = Quizzes[0];
+        expect(firstQuiz).toBeInTheDocument();
+        //const firstDesc = firstQuiz
+        expect(Quizzes[1]).toBeInTheDocument();
+        expect(
+            screen.getByText(
+                "Are your math skills better than a 5th grader? Play this to find out."
+            )
+        ).toBeInTheDocument();
     });
+    //
+    // Questions in Quizzes (can select quiz and see questions with name, body, and points)
+    // SA and MC questions (two types of questions)
+    // Correctness of Answer (can choose an answer and see if they are right)
+    // Sum points (user can see total points for a quiz)
+    // Clear Answer (users can clear their existing answers for a quiz)
+    // Publish Questions (users can publish or unpublish a question)
+    // Filter Questions (users can filter questiosn such that only published questions are shown)
+    // Edit Questions (users can edit the questions and fields of a quiz)
+    // Add Questions (users can add questions)
+    // Delete Questions (users can delete questions)
+    // Reorder Questions (users can reorder questions)
+    // Add Quizzes (users can add quizzes)
+    // Delete Quizzes (users can delete quizzes)
 });
--- src/quizzer/Quizzer.tsx
+++ src/quizzer/Quizzer.tsx
@@ -1,5 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import "../App.css";
+import { Form } from "react-bootstrap";
+import { Quiz } from "./Interfaces/quizzes";
+import { QuizList } from "./Components/QuizList";
+import startQuizzes from "./Data/starting_quizzes.json";
+
+const QUIZZES = startQuizzes.map((quiz): Quiz => quiz as Quiz);
 
 export function Quizzer(): JSX.Element {
-    return <h3>Quizzer</h3>;
+    const [edit, setEdit] = useState<boolean>(false);
+    const [quizzes, setQuizzes] = useState<Quiz[]>(QUIZZES);
+
+    function deleteQuiz(id: number) {
+        setQuizzes(quizzes.filter((quiz: Quiz): boolean => quiz.id !== id));
+    }
+
+    function addQuiz(newQuiz: Quiz) {
+        setQuizzes([...quizzes, newQuiz]);
+    }
+
+    return (
+        <div className="App">
+            <h3>Quizzer</h3>
+            <div>
+                <Form.Check
+                    inline
+                    type="switch"
+                    id="in-edit"
+                    label={edit ? "Edit Mode" : "Play Mode"}
+                    checked={edit}
+                    onChange={(e) => setEdit(e.target.checked)}
+                />
+            </div>
+            <div>
+                <QuizList
+                    quizzes={quizzes}
+                    edit={edit}
+                    deleteQuiz={deleteQuiz}
+                    addQuiz={addQuiz}
+                ></QuizList>
+            </div>
+        </div>
+    );
 }
